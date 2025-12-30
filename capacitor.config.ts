a800b15b1ba83311cf7e8cb641fe6b1357cf9f78
betomacia <cectelevision@gmail.com>
2025-12-30 11:57:09 -0300

    iOS con Facebook SDK - build 74

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+﻿import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'es.movilive.hablacondios2',
   appName: 'Habla con Dios',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    minVersion: '14.0'
+  }
 };
 
 export default config;
